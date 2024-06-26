c64e00540b66ea3a609f2d1ec80a869460ff4eda
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-06-26 12:09:00 -0300

    managing main space

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,9 +2,12 @@
     import Carrousel from "./Carrousel.svelte";
 </script>
 
-<main>
-    <section>
-        <Carrousel/>
+<main class="flex flex-col flex-grow">
+    <section class="bg-red-400 flex-grow">
+      inner section taking remaining height
+    </section>
+    <section class="bg-green-400 flex-grow">
+      hola
     </section>
 </main>
 
