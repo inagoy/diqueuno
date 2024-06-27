a8449405245e7bca062f421d9fe66cb2034c2eb9
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-06-27 10:47:12 -0300

    reorganizing code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
-    import Main from "../components/Main.svelte";
+
 </script>
 
-<Main/>
+<main class="flex flex-col flex-grow">
+    <section class="bg-red-400 flex-grow">
+      inner section taking remaining height
+    </section>
+    <section class="bg-green-400 flex-grow">
+      hola
+    </section>
+</main>
