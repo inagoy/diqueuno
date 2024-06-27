5f658018a5edf491d21fa870cfa099029ff44229
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-06-27 17:06:57 -0300

    adding carousel

--- src/components/Carrousel.svelte
+++ src/components/Carrousel.svelte
@@ -1,6 +1,26 @@
 <script>
-
-</script>
-<div class="h-full bg-red-600">
-    <img src="/fachada.jpg" alt="Fachada" class="h-full w-full object-cover"/>
-</div>
+    import { Carousel, Indicator } from 'flowbite-svelte';
+    export const images = [
+      {
+        alt: 'Cosmic timetraveler',
+        src: '/living2.jpg',
+        title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com',
+      },
+      {
+        alt: 'Cristina Gottardi',
+        src: '/living1.jpg',
+        title: 'cristina-gottardi-CSpjU6hYo_0-unsplash.com',
+      },
+      {
+        alt: 'Johannes Plenio',
+        src: '/fachada.jpg',
+        title: 'johannes-plenio-RwHv7LgeC7s-unsplash.com',
+      },
+    ];
+  </script>
+  
+  <div class="h-full place-content-center">
+    <Carousel {images} duration="5000" imgClass="h-full object-cover" class="min-h-screen rounded-none" let:Indicators>
+        <Indicators class="z-[1]"/>
+    </Carousel>
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,17 @@
 </script>
 
 <main class="flex flex-col ">
-    <section class="flex-col flex-grow h-screen ">
-      <Carrousel/>
+    <section  id="introPage" class="relative flex-grow h-screen items-center justify-center">
+      <div class="select-none absolute inset-0 flex items-center justify-center">
+        <div class="text-center  text-4xl sm:text-5xl md:text-6xl  z-[2] " style="text-shadow: 1px 1px 2px black">
+          <!-- <h1 class="font-semibold">Bienvenidos a <span class=" text-orange-600">DiqueUNO</span></h1> -->
+          <p>
+            <br />Una Residencia exclusiva para <br />
+            <span class="text-orange-600">Estudiantes Universitarios</span>
+          </p>
+        </div>
+      </div>
+      <Carrousel />
     </section>
     <section class="border border-green-400 h-[200px]">
       hola
