f92da1d3d97d1bb246dc1787251e2bb797e0df6d
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-07-02 12:55:40 -0300

    habitaciones responsive carousel

--- src/components/Habitaciones.svelte
+++ src/components/Habitaciones.svelte
@@ -9,14 +9,14 @@
     ];
 </script>
 <div class="flex flex-row overflow-hidden px-[3rem] flex-grow items-center justify-center">
-    <div class="hidden lg:block">
-      <Gallery class="gap-2 flex flex-row" items={images} imgClass="rounded-sm object-cover max-h-[800px] w-full" />
-    </div>
-    <div class="h-full place-content-center lg:hidden">
-      <Carousel {images} duration="5000" imgClass="h-full object-cover" class="min-h-screen rounded-none z-[1]" let:Indicators>
-          <Indicators class="z-[1]"/>
-      </Carousel>
-    </div>
+  <div class="hidden flex-grow lg:block">
+    <Gallery class="gap-2 flex flex-row" items={images} imgClass="rounded-sm object-cover max-h-[800px] w-full" />
+  </div>
+  <div class="relative flex-grow h-full lg:hidden">
+    <Carousel {images} duration="5000" imgClass="h-full object-cover" class=" rounded-none z-[1]" let:Indicators>
+      <Indicators class="z-[1]" />
+    </Carousel>
+  </div>
     <div class="flex flex-col justify-center items-center text-sm lg:text-base sm:leading-loose opacity-90  pl-5 ">
       <p class="text-left text-white">
         Nuestras habitaciones son <span class="text-orange-400 font-semibold">individuales</span> y todas cuentan con:
