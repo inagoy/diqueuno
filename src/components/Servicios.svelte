4846e5abab69f1ece0c019aeca87e41e0e5943d8
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-07-14 10:23:06 -0300

    change in style

--- src/components/Servicios.svelte
+++ src/components/Servicios.svelte
@@ -26,9 +26,9 @@
 <div class="flex flex-grow">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-10 opacity-90">
         {#each cards as card}
-        <Card class="shadow-2xl bg-gray-100 flex flex-col items-center justify-center">
+        <Card class="shadow-2xl bg-white flex flex-col items-center justify-center">
             <h5 class="mb-2 text-lg md:text-2xl font-semibold text-gray-900 text-center">{card.name}</h5>
-            <p class="mt-3 text-sm md:text-base font-normal text-gray-500 dark:text-gray-400 text-center">{card.text}</p>
+            <p class="mt-3 text-sm md:text-base font-normal text-gray-500 text-center">{card.text}</p>
             <div class="text-center lg:text-9xl md:text-7xl text-5xl mt-5 flex items-center justify-center flex-grow">
                 <i class="fa {card.icon} text-primary-600" aria-hidden="true"></i>
             </div>
